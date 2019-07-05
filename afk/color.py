302e9919f2b6cdb136576c14b8b3164d0aff3700
Mudream <mudream4869@gmail.com>
2019-07-05 20:32:47 +0800

    afk

---
+++ afk/color.py
@@ -0,0 +1,19 @@
+class Colors: 
+    reset = '\033[0m'
+    colors = { 
+        'black': '\033[30m',
+        'red': '\033[31m',
+        'green': '\033[32m',
+        'orange': '\033[33m',
+        'blue': '\033[34m',
+        'purple': '\033[35m',
+        'cyan': '\033[36m',
+        'light_gray': '\033[37m',
+        'dark_gray': '\033[90m',
+        'light_red': '\033[91m',
+        'light_green': '\033[92m',
+        'yellow': '\033[93m',
+        'light_blue': '\033[94m',
+        'pink': '\033[95m',
+        'light_cyan': '\033[96m'
+    }
